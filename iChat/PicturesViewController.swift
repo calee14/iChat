24cc89bb3e65517565abcfb715921880ba5c2dd8
calee14 <caplee1000@gmail.com>
2016-12-27 12:15:21 -0800

    Clean slate and pushin

--- iChat/PicturesViewController.swift
+++ iChat/PicturesViewController.swift
@@ -1,19 +1,19 @@
 //
-//  PicsViewController.swift
-//  iChat
+//  PicturesViewController.swift
+//  
 //
 //  Created by Cappillen on 12/27/16.
-//  Copyright © 2016 Cappillen. All rights reserved.
+//
 //
 
 import UIKit
 import Firebase
 
-class PicsViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class PicturesViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var descriptionText: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
     
     var imagePicker = UIImagePickerController()
     
@@ -22,34 +22,51 @@ class PicsViewController: UIViewController, UIImagePickerControllerDelegate, UIN
 
         imagePicker.delegate = self
     }
-
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
         imageView.image = image
+        
         imageView.backgroundColor = UIColor.clear
+        
         imagePicker.dismiss(animated: true, completion: nil)
     }
-    
+
     @IBAction func cameraTapped(_ sender: Any) {
+        
         imagePicker.sourceType = .savedPhotosAlbum
+        
         imagePicker.allowsEditing = false
+        
         present(imagePicker, animated: true, completion: nil)
     }
+    
     @IBAction func nextTapped(_ sender: Any) {
-        performSegue(withIdentifier: "selectUserSegue", sender: nil)
-    }
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        nextButton.isEnabled = false
+        
         let imagesFolder =
             FIRStorage.storage().reference().child("images")
         
-        let imageData = UIImagePNGRepresentation(imageView.image!)!
+        let imageData = UIImageJPEGRepresentation(imageView.image!, 0.1)!
         
-        imagesFolder.child("images.PNG").put(imageData, metadata: nil, completion: {(metadata, error) in
+        imagesFolder.child("\(NSUUID().uuidString).jpg").put(imageData, metadata: nil, completion: {(metadata, error) in
             print("We tried to upload")
+            
             if error != nil {
-                print("We had an error:\(error)")
+                
+                print("We hand an error:\(error)")
+                
+            } else {
+                
+                print(metadata?.downloadURL())
+                
+                self.performSegue(withIdentifier: "selectUserSegue", sender: nil)
             }
         })
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
     }
     
---
+++ iChat/SelectUserViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SelectUserViewController.swift
+//  iChat
+//
+//  Created by Cappillen on 12/27/16.
+//  Copyright © 2016 Cappillen. All rights reserved.
+//
+
+import UIKit
+
+class SelectUserViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+}
